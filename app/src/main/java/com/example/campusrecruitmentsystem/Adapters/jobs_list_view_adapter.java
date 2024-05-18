f277a29018b7bf561c1c0f10c54d4f904e85ca24
waqasmehmood44 <56045126+waqasmehmood44@users.noreply.github.com>
2024-05-19 01:34:37 +0500

    vxcvxcvxcvxc

--- app/src/main/java/com/example/campusrecruitmentsystem/Adapters/jobs_list_view_adapter.java
+++ app/src/main/java/com/example/campusrecruitmentsystem/Adapters/jobs_list_view_adapter.java
@@ -2,7 +2,9 @@ package com.example.campusrecruitmentsystem.Adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,11 +12,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.campusrecruitmentsystem.R;
 import com.example.campusrecruitmentsystem.ViewHolders.jobs_list_view_holder;
 import com.example.campusrecruitmentsystem.Models.post_job_model;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.List;
 
 public class jobs_list_view_adapter extends RecyclerView.Adapter<jobs_list_view_holder> {
     Context context;
+    DatabaseReference reference,reference1;
     List<post_job_model> itemList;
 
     public jobs_list_view_adapter(Context context, List<post_job_model> itemList) {
@@ -35,6 +41,20 @@ public class jobs_list_view_adapter extends RecyclerView.Adapter<jobs_list_view_
     holder.job_eligiblity.setText(itemList.get(position).getEligibility());
     holder.job_location.setText(itemList.get(position).getLocation());
     holder.job_description.setText(itemList.get(position).getDescription());
+    holder.delete_icon.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            reference = FirebaseDatabase.getInstance().getReference().child("Jobs").child(itemList.get(position).getRec_id()).child(itemList.get(position).getJob_id());
+            reference1 = FirebaseDatabase.getInstance().getReference().child("Student Jobs List").child(itemList.get(position).getJob_id());
+            reference1.removeValue();
+            reference.removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void unused) {
+                    Toast.makeText(context, "Job Deleted Successfully", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    });
     }
 
     @Override
--- app/src/main/java/com/example/campusrecruitmentsystem/ViewHolders/jobs_list_view_holder.java
+++ app/src/main/java/com/example/campusrecruitmentsystem/ViewHolders/jobs_list_view_holder.java
@@ -1,6 +1,7 @@
 package com.example.campusrecruitmentsystem.ViewHolders;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,6 +15,7 @@ public class jobs_list_view_holder extends RecyclerView.ViewHolder {
     public TextView job_eligiblity;
     public TextView job_location;
     public TextView job_description;
+    public ImageView delete_icon;
     public jobs_list_view_holder(@NonNull View itemView) {
         super(itemView);
         job_name = itemView.findViewById(R.id.job_name);
@@ -21,5 +23,6 @@ public class jobs_list_view_holder extends RecyclerView.ViewHolder {
         job_eligiblity = itemView.findViewById(R.id.job_eligiblity);
         job_location = itemView.findViewById(R.id.job_location);
         job_description = itemView.findViewById(R.id.job_description);
+        delete_icon = itemView.findViewById(R.id.delete_icon);
     }
 }
